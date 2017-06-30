2d0d1766e8ef3bd50eb61158ebec63d8339ab7ed
Otto Nagengast <otto.nagengast@gmail.com>
2017-06-30 14:51:32 -0700

    Checkpoint 2

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,4 +1,34 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom';  
 
-ReactDOM.render(<p> React lives! </p>, document.getElementById('root')); 
+var dummyData = ['Buy bagels', 'Learn code', 'Call home']
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props); 
+  }
+  render() {
+  	return (
+  		<div>
+  			<ul>
+  				{dummyData.map((task) => 
+  					<Todo task = {task} />
+  				)}
+  			</ul>
+  		</div>
+  	)
+  } 
+}   
+
+class Todo extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+	render() {
+		return (
+			<li> {this.props.task}   <button type="button"> X </button> </li>
+		)
+	}
+ }
+
+ReactDOM.render(<TodoList />, document.getElementById('root')); 
