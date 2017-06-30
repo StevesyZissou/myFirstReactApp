128918082d8cba832f6c07e59dc55c08ab38c28d
Otto Nagengast <otto.nagengast@gmail.com>
2017-06-30 15:54:40 -0700

    Updates

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,17 +1,31 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom';  
 
-var dummyData = ['Buy bagels', 'Learn code', 'Call home']
+var dummyData = [
+{taskText: 'Buy bagels', completed: false}, 
+{taskText: 'Learn code', completed: false}, 
+{taskText: 'Call home', completed: true}, 
+{taskText: 'Play in the NBA', completed: false}, 
+{taskText: 'Finish middle school', completed: true}
+]
 
 class TodoApp extends React.Component {
 	constructor(props) {
 		super(props)
+		this.state = {
+    	todos: []
+    }
 	}
+
+	componentDidMount() {
+		this.setState({todos: dummyData})
+	}
+
 	render() {
 		return (
-			<div>
+			<div className = "TodoApp">
 				<InputLine />
-				<TodoList tasks = {dummyData} /> 
+				<TodoList todos = {this.state.todos} /> 
 			</div>
 		)
 	}
@@ -23,7 +37,7 @@ class InputLine extends React.Component {
 	}
 	render() {
 		return (
-			<form>
+			<form className = "InputLine">
 				<label> Add a task: </label>
 				<input type="text" placeholder="What needs to be done?" /> <input type="Submit" value="Add to your todo list" />
 			</form>
@@ -35,20 +49,22 @@ class TodoList extends React.Component {
   constructor(props) {
     super(props); 
   }
+
   render() {
   		return (
 	  		<div>
 	  			<ul>
-	  				{this.props.tasks.map((task) => 
-	  					<Todo task = {task} />
+	  				{this.props.todos.map((task) => 
+	  					<Todo 
+	  					taskText = {task.taskText}
+	  					completed = {task.completed}
+	  					/>
 	  				)}
 	  			</ul>
 	  		</div>
   	)
   } 
-}   
-
-{() => this.handleSubmit} 
+}    
 
 class Todo extends React.Component {
 	constructor(props) {
@@ -56,7 +72,15 @@ class Todo extends React.Component {
 	}
 	render() {
 		return (
-			<li> {this.props.task}   <button type="button"> X </button> </li>
+			(this.props.completed) ? 
+			<li> 
+				<strike> {this.props.taskText}  </strike> 
+				<button type="button">   X </button> 
+			</li>
+			: <li> 
+				{this.props.taskText}  
+				<button type="button">   X </button> 
+			</li>
 		)
 	}
  }
