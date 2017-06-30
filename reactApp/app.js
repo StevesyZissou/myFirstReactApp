f9d6f1e1ad289b0a402ed73ba46494f21a43d5c7
Otto Nagengast <otto.nagengast@gmail.com>
2017-06-30 15:11:10 -0700

    Full static

--- reactApp/app.js
+++ reactApp/app.js
@@ -3,22 +3,52 @@ import ReactDOM from 'react-dom';
 
 var dummyData = ['Buy bagels', 'Learn code', 'Call home']
 
+class TodoApp extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+	render() {
+		return (
+			<div>
+				<InputLine />
+				<TodoList tasks = {dummyData} /> 
+			</div>
+		)
+	}
+}
+
+class InputLine extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+	render() {
+		return (
+			<form>
+				<label> Add a task: </label>
+				<input type="text" placeholder="What needs to be done?" /> <input type="Submit" value="Add to your todo list" />
+			</form>
+		)
+	}
+}
+
 class TodoList extends React.Component {
   constructor(props) {
     super(props); 
   }
   render() {
-  	return (
-  		<div>
-  			<ul>
-  				{dummyData.map((task) => 
-  					<Todo task = {task} />
-  				)}
-  			</ul>
-  		</div>
+  		return (
+	  		<div>
+	  			<ul>
+	  				{this.props.tasks.map((task) => 
+	  					<Todo task = {task} />
+	  				)}
+	  			</ul>
+	  		</div>
   	)
   } 
 }   
+
+{() => this.handleSubmit} 
 
 class Todo extends React.Component {
 	constructor(props) {
@@ -31,4 +61,4 @@ class Todo extends React.Component {
 	}
  }
 
-ReactDOM.render(<TodoList />, document.getElementById('root')); 
+ReactDOM.render(<TodoApp />, document.getElementById('root')); 
